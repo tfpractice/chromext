1c1222f5974cff3c13e6030c26741c87dede4cfe
tfpractice <jackson.brebnor@gmail.com>
2018-02-22 20:46:17 -0500

    compare domains

--- src/shared/components/main/App.js
+++ src/shared/components/main/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tabs } from '../../utils';
+
+const queryTabs = () => {
+  const chrome = window.chrome;
+
+  // console.log('chrome', chrome);
+  chrome.tabs.query({ currentWindow: true }, tabs => {
+    console.log('before tabs', tabs);
+    tabs.sort(Tabs.compUrl);
+    console.log('after tabs', tabs);
+  });
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <button onClick={queryTabs}>fliptabs</button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">To get started, edit and save to reload.</p>
       </div>
     );
   }
