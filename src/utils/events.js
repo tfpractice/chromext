3711864a43175915230da079e60ab08df62aec5b
tfpractice <jackson.brebnor@gmail.com>
2018-03-05 16:48:17 -0500

    updates tabs on load

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,12 +1,12 @@
 import { Actions } from '../store';
 
 const { Tabs } = Actions;
-const { createTab, dropTab } = Tabs;
+const { createTab, dropTab, getTabs } = Tabs;
 const registerEvents = chrome => store => {
   chrome.tabs.onCreated.addListener(t => store.dispatch(createTab(t)));
 
   chrome.tabs.onRemoved.addListener(id => store.dispatch(dropTab({ id })));
-
+  store.dispatch(getTabs());
   return store;
 };
 
